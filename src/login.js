98e59128ebe3aca5acb6bfe1180d58cb457daf5f
SaiyaraOishi <saiyara.oishi@gmail.com>
2022-08-05 14:21:49 +0600

    Error message in <Login/>

--- src/login.js
+++ src/login.js
@@ -7,10 +7,14 @@ export default function Login(){
     const [name,setName]=useState("");
     const {setName:setContextName} = useContext(UserContext);
 
+    const formIsValid=name.trim().length>0;
+
     const navigate=useNavigate();
     function handleSubmit(){
+        if(formIsValid){
         setContextName(name);
         navigate("/dashboard");
+        }
     }
 
     const handleNameChange = (e) => {
@@ -26,6 +30,7 @@ export default function Login(){
         <form>
             <div className="text-center mt-20">
             <input className='text-center border border-cyan-700 rounded-md py-2' type="text" placeholder="Your name" name="name" id="name" value={name} onChange={handleNameChange}/>
+            <div className="text-red-600">{!formIsValid && <p>*Name is required</p>}</div>
             </div>
             <div className="mt-28 text-center ">
                 <button type="submit" className="bg-blue-500 px-4 py-2 text-white rounded-md" onClick={handleSubmit}>Submit</button>
