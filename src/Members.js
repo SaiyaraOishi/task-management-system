6489396d0d881b4e9c1aa3c19ea7670222831d6a
SaiyaraOishi <saiyara.oishi@gmail.com>
2022-08-11 17:20:29 +0600

    Design changes

--- src/Members.js
+++ src/Members.js
@@ -24,7 +24,7 @@ export default function Members(){
         <p className="text-lg mt-10">You will find all members here.</p>
         <div className="mt-4 text-2xl font-bold">Here are all members:</div>
         <button className="bg-blue-500 px-4 py-2 text-white rounded-md" onClick={onAddMember}>Add New</button>
-        <div>
+        <div className="w-36">
             <ol>
                 {memberlist.map(member=>(
                     <div>
@@ -32,7 +32,7 @@ export default function Members(){
                         <div className=" underline text-blue-600">
                         <p onClick={()=>handleUpdateMember(member.id)}>{member.name}</p>
                         </div>
-                        <p className="text-blue-600"> &nbsp; &nbsp; {tasklist.filter(task=>(task.member===member.name)).length}tasks</p>
+                        <p className="text-blue-600 ml-auto underline">{tasklist.filter(task=>(task.member===member.name)).length}tasks</p>
                     </li>
                     </div>
                 ))}
--- src/Tasks.js
+++ src/Tasks.js
@@ -24,15 +24,15 @@ export default function Tasks(){
         <p className="text-lg mt-10">You will find all tasks here.</p>
         <div className="mt-4 text-2xl font-bold">Here is all tasks:</div>
         <button className="bg-blue-500 px-4 py-2 text-white rounded-md" onClick={onAddTask}>Add New</button>
-        <div>
+        <div className="w-36">
         <ol>
             {tasklist.map(task=>(
                 <div className="">
                 <li className="flex">
-                    <div className=" underline text-blue-600">
+                    <div className="underline text-blue-600">
                         <p onClick={()=>handleUpdate(task.id)}>{task.title}</p>
                     </div>
-                    <p className="text-blue-600"> &nbsp;&nbsp; {task.member}</p>
+                    <p className="text-blue-600 ml-auto underline">{task.member}</p>
                     {/* <button onClick={()=>handleUpdate(task.id)}>Edit</button> */}
                 </li>
                 </div>
