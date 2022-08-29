5fd980a11085fb14e185962232a2e7f1ff554036
SaiyaraOishi <saiyara.oishi@gmail.com>
2022-08-29 17:00:30 +0600

    redux-persist and localstorage implemented

--- src/App.js
+++ src/App.js
@@ -7,26 +7,29 @@ import AddMember from "./components/AddMember";
 import AddTask from "./components/AddTask";
 import UpdateMember from "./components/UpdateMember";
 import UpdateTask from "./components/UpdateTask";
-import {Provider} from "react-redux";
-import store from "./redux/store";
+import { Provider } from "react-redux";
+import store, { persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 function App() {
   return (
     <>
-    <Provider store={store}>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Login/>}/>
-      <Route path="/dashboard" element={<Dashboard/>}/>
-      <Route path="/task" element={<Tasks/>}/>
-      <Route path="/member" element={<Members/>}/>
-      <Route path="/addtask" element={<AddTask/>}/>
-      <Route path="/addmember" element={<AddMember/>}/>
-      <Route path="/task/:id" element={<UpdateTask/>}/>
-      <Route path="/member/:id" element={<UpdateMember/>}/>
-    </Routes>
-    </BrowserRouter>
-    </Provider>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/task" element={<Tasks />} />
+              <Route path="/member" element={<Members />} />
+              <Route path="/addtask" element={<AddTask />} />
+              <Route path="/addmember" element={<AddMember />} />
+              <Route path="/task/:id" element={<UpdateTask />} />
+              <Route path="/member/:id" element={<UpdateMember />} />
+            </Routes>
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
     </>
   );
 }
