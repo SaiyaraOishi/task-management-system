ae79d8e244526888a8132fb0b0815b05625d65af
SaiyaraOishi <saiyara.oishi@gmail.com>
2022-08-31 18:12:32 +0600

    useFormik applied to <Login/>

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,26 +1,29 @@
-import { useState } from "react";
+import { useFormik } from "formik";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { addName } from "../redux/user/actions";
 
 export default function Login() {
 
-    const [name, setName] = useState("");
     const dispatch = useDispatch();
-
-    const formIsValid = name.trim().length > 0;
-
     const navigate = useNavigate();
-    function onSubmitClick() {
-        if (formIsValid) {
-            dispatch(addName(name));
+
+    const formik = useFormik({
+        initialValues: {
+            name: ""
+        },
+        validate: values => {
+            let errors = {};
+            if(!values.name){
+                errors.name="*Name is required";
+            }
+            return errors;
+        },
+        onSubmit: values => {
+            dispatch(addName(values.name));
             navigate("/dashboard");
         }
-    }
-
-    const handleNameChange = (e) => {
-        setName(e.target.value);
-    }
+    });
 
     return (
         <>
@@ -28,13 +31,13 @@ export default function Login() {
                 <img src="logo192.png" alt="" />
             </div>
             <div className="mt-2 text-2xl bold text-cyan-700 text-center">Task Management</div>
-            <form>
+            <form onSubmit={formik.handleSubmit}>
                 <div className="text-center mt-20">
-                    <input className='text-center border border-cyan-700 rounded-md py-2' type="text" placeholder="Your name" name="name" id="name" value={name} onChange={handleNameChange} />
-                    <div className="text-red-600">{!formIsValid && <p>*Name is required</p>}</div>
+                    <input className='text-center border border-cyan-700 rounded-md py-2' type="text" placeholder="Your name" name="name" id="name" value={formik.name} onChange={formik.handleChange} />
+                    {formik.errors.name ? <div className="text-red-600">{formik.errors.name}</div> : null}
                 </div>
                 <div className="mt-28 text-center ">
-                    <button type="submit" className="bg-blue-500 px-4 py-2 text-white rounded-md" onClick={onSubmitClick}>Submit</button>
+                    <button type="submit" className="bg-blue-500 px-4 py-2 text-white rounded-md">Submit</button>
                 </div>
             </form>
         </>
