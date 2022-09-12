304c35562ec34aa05822f25b3e6d39e0f05e0511
SaiyaraOishi <saiyara.oishi@gmail.com>
2022-09-12 22:27:14 +0600

    login done with custom api

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,9 @@
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { addName } from "../redux/user/actions";
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
 import FormikControl from "../Formik/FormikControl";
+import loginUser from "../redux/user/thunk/loginUser";
 
 
 export default function Login() {
@@ -13,13 +13,18 @@ export default function Login() {
 
     const initialValues = {
         name: "",
+        email: "",
+        password: ""
     };
     const validationSchema = Yup.object({
         name: Yup.string().required("*Required"),
+        email: Yup.string().required("*Required"),
+        password: Yup.string().required("*Required"),
     });
-    const onSubmit = (values) => {
-        dispatch(addName(values.name));
-        navigate("/dashboard");
+    const onSubmit = async (values) => {
+        // dispatch(addName(values.name));
+        // navigate("/dashboard");
+        await loginUser(navigate, dispatch, values.name, values.email, values.password);
     };
 
     return (
@@ -28,47 +33,54 @@ export default function Login() {
                 <img src="logo192.png" alt="" />
             </div>
             <div className="mt-2 text-2xl bold text-cyan-700 text-center">Task Management</div>
-                <div className="text-center mt-20">
-                    <Formik
-                        initialValues={initialValues}
-                        validationSchema={validationSchema}
-                        onSubmit={onSubmit}
-                        >
-                        {(formik) => {
-                            return (
-                                <Form>
-                                    <div>
-                                        <FormikControl
-                                            className="text-center border border-cyan-700 rounded-md py-2"
-                                            control="input"
-                                            type="text"
-                                            name="name"
-                                            placeholder="Enter username"
-                                        />
-                                        <FormikControl
-                                            className="mt-4 text-center border border-cyan-700 rounded-md py-2"
-                                            control="input"
-                                            type="password"
-                                            name="password"
-                                            placeholder="Enter password"
-                                        />
+            <div className="text-center mt-20">
+                <Formik
+                    initialValues={initialValues}
+                    validationSchema={validationSchema}
+                    onSubmit={onSubmit}
+                >
+                    {(formik) => {
+                        return (
+                            <Form>
+                                <div>
+                                    <FormikControl
+                                        className="text-center border border-cyan-700 rounded-md py-2"
+                                        control="input"
+                                        type="text"
+                                        name="name"
+                                        placeholder="Enter username"
+                                    />
+                                    <FormikControl
+                                        className="mt-4 text-center border border-cyan-700 rounded-md py-2"
+                                        control="input"
+                                        type="text"
+                                        name="email"
+                                        placeholder="Enter email"
+                                    />
+                                    <FormikControl
+                                        className="mt-4 text-center border border-cyan-700 rounded-md py-2"
+                                        control="input"
+                                        type="password"
+                                        name="password"
+                                        placeholder="Enter password"
+                                    />
 
-                                        <button
+                                    <button
                                         className="bg-blue-500 mt-20 px-4 py-2 text-white rounded-md"
-                                            type="submit"
-                                            disabled={!formik.isValid}
-                                        >
-                                            Submit
-                                        </button>
-                                    </div>
-                                </Form>
+                                        type="submit"
+                                        disabled={!formik.isValid}
+                                    >
+                                        Submit
+                                    </button>
+                                </div>
+                            </Form>
 
-                            );
+                        );
 
-                        }}
-                        </Formik>
-                </div>
-                
+                    }}
+                </Formik>
+            </div>
+
         </>
     )
 }
---
+++ src/redux/user/thunk/loginUser.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { addName } from "../actions";
+
+const base_url = "http://localhost:9001";
+
+const loginUser = async (navigate, dispatch, name, email, password) => {
+    try{
+        const response = await axios({
+            url: `${base_url}/public/login`,
+            method: "POST",
+            data: {
+                email,
+                password
+            }
+        });
+        console.log(response.data);
+        navigate("/dashboard");
+        dispatch(addName({name,email,password}));
+    }
+    catch(error){
+        console.error(error);
+    }
+}
+
+export default loginUser;
